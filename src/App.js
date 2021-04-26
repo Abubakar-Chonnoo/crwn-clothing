8a2c9171557a177d8db0031a7de850bb756ca7f1
Abubakar Siddick Khan Chonnoo <Abubakar.chonnoo@hangarww.com>
2021-04-26 16:01:40 +0400

    update our menu-itesm to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage-Compo';
+import { Switch, Route } from 'react-router-dom';
+
+
+const Hats = () => (
+  <div>
+  <h1>Hello</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    // <div>
+    //   <HomePage />
+    // </div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route  path='/hats' component={Hats} />
+    </Switch>
   );
 }
 
