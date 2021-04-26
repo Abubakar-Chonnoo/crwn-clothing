e2e0f25004e7929148d2124b793d45cddc5281bf
Abubakar Siddick Khan Chonnoo <Abubakar.chonnoo@hangarww.com>
2021-04-26 10:52:44 +0400

    base structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HomePage from './components/homepage/homepage-Compo';
+import Homepage from './components/homepage/homepage-Compo';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
